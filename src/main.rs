a779fe19fcce0d65010209fba06204cf32ae7f7d
Cannafox <gklodkowski@gmail.com>
2024-10-07 20:07:38 +0200

    game

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guessing Game");
+
+    let random_number: u32 = rand::thread_rng().gen_range(1..=10);
+
+    loop {
+        println!("I am thinking about number {}, about what number I am thinking?", random_number);
+        println!("Enter your number: ");
+
+        let mut user_guess = String::new();
+
+        io::stdin()
+            .read_line(&mut user_guess)
+            .expect("Failed to get a number!");
+
+        // Conversion, user_guess is mutable, so you can assign it with different type
+        let user_guess: u32 = match user_guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match user_guess.cmp(&random_number) {
+            Ordering::Less => println!("Your number is smaller!"),
+            Ordering::Greater => println!("Your number is bigger!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+                },
+        }
+    }
+
+
+}
